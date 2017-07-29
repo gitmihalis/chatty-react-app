5aaeabb091b20a497c69f723c89b99845e3462bc
gitmihalis <mihalisfthenos@icloud.com>
2017-07-29 15:48:46 +0000

    refactor incoming message handler in ws server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,14 +1,13 @@
 // Server 
-// TODO :: collect usersnames and add to client object
 const express = require('express')
 const SockectServer = require('ws').Server
-
 // Universal unique identifiers for messages
 const uuid = require('node-uuid')
-
+// Random colors will be assigned to connected clients
+// - randomColor([saturation, value]); 
+const randomColor = require('random-color');
 // Set the port to 3001
 const PORT = 3001
-
 // Create a new express server
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
@@ -16,41 +15,26 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`))
 // Create a web sockets server
 const wss = new SockectServer({ server })
-
-// TODO Currently connected clients
+// Keep info on currently connected clients
 const CLIENTS = {}
+// Keep list of possible colors assignable to connected clients
+
+
 // A callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+// the `client` parameter in the callback.
 wss.on('connection', (client) => {
 
-  const cId = uuid()
+  const uniqueId = uuid()
 
-	clientConnected(CLIENTS, cId)
+	clientConnected(CLIENTS, uniqueId)
+
 	//  A callback for when a client closes the socket. This usually means they closed their browser.
-  client.on('close', () => clientDisconnected(CLIENTS, cId) )
+  client.on('close', () => clientDisconnected(CLIENTS, uniqueId) )
 
-  client.on('message', (incoming) => {
- 	  console.log('incoming message: ', incoming)
-
- 		const data = JSON.parse(incoming)
-
- 		switch(data.type) {
- 			case "postMessage":
- 				data.id = uuid()
- 				data.type = 'incomingMessage'
- 				wss.broadcast(JSON.stringify(data))
- 				break
- 			case "postNotification":
- 				data.id = uuid()
- 				data.type = 'incomingNotification'
- 				wss.broadcast(JSON.stringify(data))
- 				break
- 			default:
- 				console.error('Unkown data type ', data.type)
- 		}
- 	})
+  client.on('message', (incoming) => handleIncoming(incoming))
 })
+
 // Broadcast - Goes through each client and sends message data
 wss.broadcast = function(data) {
   wss.clients.forEach(function(client) {
@@ -59,6 +43,7 @@ wss.broadcast = function(data) {
     }
   })
 }
+// ==================================== HANDLERS ====
 // A client is added to clients on a connection event
 function clientConnected(clients, clientId) {
   clients[clientId] = {
@@ -97,6 +82,25 @@ function clientDisconnected(clients, clientId) {
   console.log('[^^ clientsArr] ', clientsArr)
   wss.broadcast(JSON.stringify(message))
   console.log(`<< client disconnected`)
+}
+
+function handleIncoming(message) {
+  const data = JSON.parse(message)
+  
+  switch(data.type) {
+  case "postMessage":
+    data.id = uuid()
+    data.type = 'incomingMessage'
+    wss.broadcast(JSON.stringify(data))
+    break
+  case "postNotification":
+    data.id = uuid()
+    data.type = 'incomingNotification'
+    wss.broadcast(JSON.stringify(data))
+    break
+  default:
+    console.error('Unkown data type ', data.type)
+  }
 }
 
 
